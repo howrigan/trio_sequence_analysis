b2a665d48b40a48f8050a21fd22b98416008638c
Daniel <howrigan@broadinstitute.org>
2016-07-14 13:01:50 -0400

    edited files

--- vep_annotation.py
+++
@@ -1,7 +0,0 @@
-python /psych/genetics_data/howrigan/projects/taiwan_trio/scripts/run_lof_annotation.py \
--i /seq/dax/TaiTrios_AllWaves_08.2015/Exome/v1/TaiTrios_AllWaves_08.2015.site_only_plus_mixins.vcf.gz \
--o /psych/genetics_data/howrigan/projects/taiwan_trio/data/annot/ \
---memory 8 \
--s 15000 \
--q hour
-
