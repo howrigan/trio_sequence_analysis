455a3133eea5b0cf2a8d0377b62bb846a2f3bc91
Daniel <howrigan@broadinstitute.org>
2019-02-14 17:12:41 -0500

    removed earlier files

--- TaiTrios_AllWaves_08.2015_de_novo_finder_trio1698.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-python \
-/psych/genetics_data/howrigan/projects/taiwan_trio/scripts/de_novo_finder_3.py \
-/seq/dax/TaiTrios_AllWaves_08.2015/Exome/v1/TaiTrios_AllWaves_08.2015.vcf.gz \
-/psych/genetics_data/howrigan/projects/taiwan_trio/data/Taiwanese_Trio_AllWaves_08.2015_confirmedTrios_wholeBloodProbands.fam \
-/humgen/atgu1/fs03/wip/kaitlin/all_ESP_counts_5.28.13.txt > \
-/psych/genetics_data/howrigan/projects/taiwan_trio/denovo_data/TaiTrios_AllWaves_08.2015.dnm
--- run_TDT_CC.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-python TDT_CC.py doTDT \
-/seq/dax/TaiTrios_AllWaves_08.2015/Exome/v1/TaiTrios_AllWaves_08.2015.vcf.gz \
-/psych/genetics_data/howrigan/projects/taiwan_trio/data/Taiwanese_Trio_AllWaves_08.2015_confirmedTrios_uncontaminatedTrios.fam \
-/psych/genetics_data/howrigan/projects/taiwan_trio/transmission_data/variant_transmission/TaiTrios_AllWaves_08.2015_n1142.txt \
---pl 25 --ab_Ref 0.05 --ab_Het 0.2 --ab_Alt 0.95 --gq_Par 25 --gq_Kid 25
-
